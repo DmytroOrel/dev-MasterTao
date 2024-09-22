b006bdd507e5687358b729c99a83a7cfe0c6e188
Mac(Dima) <dmitroorel@MacBook-Pro-MacDima.local>
2024-09-23 00:04:41 +0300

    banner

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,1 +1,43 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+  const cat = {
+    name: "Murka",
+    old: 3,
+    color: "white",
+    parents: {
+      mother: "Basya",
+      father: "Arsen"
+    }
+  }
+  const dog = {
+    name: "Mila",
+    old: 1,
+    color: "grey",
+    parents: {
+      mother: "Senna",
+      old: 3,
+      father: "Jack",
+      old: 4
+    }
+  }
   
