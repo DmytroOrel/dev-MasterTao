e2c4ae025671f45ed0c2493d1424b2a37a017429
Mac(Dima) <dmitroorel@MacBook-Pro-MacDima.local>
2024-09-25 16:55:55 +0300

    origin main

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,30 +1,21 @@
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-    navigation:{
-      enabled: false,
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-
-    breakpoints: {
-      992: {
-        navigation: {
-          enabled: true,
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      },
-    }
-  });
-
-
-  const cat = {
+const cat = {
     name: "Murka",
     old: 3,
     color: "white",
